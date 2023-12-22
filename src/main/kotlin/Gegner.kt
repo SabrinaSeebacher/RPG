4e263fecf5577dc78535d3fe42d7acca866c0522
sabrina <sabrina.seebacher@gmail.com>
2023-12-22 08:57:20 +0100

    RPG

--- src/main/kotlin/Gegner.kt
+++ src/main/kotlin/Gegner.kt
@@ -1,18 +1,18 @@
 
 import kotlin.random.Random
 
-//  offenen Klasse für Gegner
+
 open class Gegner(var name: String, open var health: Int) {
     // Methode, um Schaden am Gegner zu verursachen
     fun takeDamage(damage: Int) {
-        // Reduziere die Lebenspunkte des Gegners um den verursachten Schaden
+
         health -= damage
         // Überprüfe, ob der Gegner noch Lebenspunkte hat
         if (health > 0) {
-            // Wenn der Gegner noch lebt, gib eine Nachricht aus
+            println()
             println("$name hat $damage Schaden erlitten und hat noch $health Lebenspunkte.")
         } else {
-            // Wenn der Gegner besiegt wurde, gib eine entsprechende Nachricht aus
+            println()
             println("$name wurde besiegt!")
         }
     }
@@ -23,11 +23,11 @@ open class Gegner(var name: String, open var health: Int) {
         val randomAttackIndex = Random.nextInt(1, 5)
         // Zufällige Generierung von Schaden zwischen 8 und 15
         val damage = Random.nextInt(8, 15)
-        // Ausgabe einer Nachricht über den Angriff
+
         println("$name greift ${held.name} mit Angriff $randomAttackIndex an und verursacht $damage Schaden.")
-        // Reduziere die Lebenspunkte des Helden um den verursachten Schaden
+
         held.health -= damage
-        // Ausgabe der verbleibenden Lebenspunkte des Helden
+        // verbleibenden Lebenspunkte des Helden
         println("${held.name} hat noch ${held.health} Lebenspunkte.")
     }
 }
--- src/main/kotlin/Held.kt
+++ src/main/kotlin/Held.kt
@@ -2,18 +2,17 @@ import kotlin.random.Random
 
 
 
-// Definition einer offenen Klasse für Helden
 open class Held(val name: String) {
     // Lebenspunkte des Helden, standardmäßig auf 100 gesetzt
     var health = 100
     // Liste von Angriffen, die der Held ausführen kann
-    val attacks = listOf("Angriff 1", "Angriff 2", "Angriff 3", "Angriff 4")
-    // Inventar des Helden mit standardmäßigen Gegenständen
+    val attacks = listOf("Feuerschlag", "Axthieb", "", "Angriff 4")
+    // Inventar/Beutel des Helden mit standardmäßigen Gegenständen
     val inventory = mutableListOf("Heilungstrank", "Booster", "Schutzzauber")
 
     // Methode für den Angriff auf einen Gegner
     fun attack(enemy: Gegner) {
-        // Ausgabe einer Aufforderung zur Auswahl einer Attacke
+
         println("$name, wähle deine Attacke:")
         // Ausgabe der verfügbaren Angriffe mit ihren Indexnummern
         attacks.forEachIndexed { index, attack -> println("$index. $attack") }
@@ -21,12 +20,12 @@ open class Held(val name: String) {
         // Einlesen des Angriffsindex vom Benutzer
         val attackIndex = readln().toIntOrNull()
 
-        // Überprüfung, ob die Eingabe gültig ist und im Bereich der Angriffe liegt
+        // Überprüfung Eingabe gültig ist und im Bereich der Angriffe liegt
         if (attackIndex != null && attackIndex in attacks.indices) {
             // Auswahl des ausgewählten Angriffs
             val selectedAttack = attacks[attackIndex]
 
-            // Bestimmung des Schadens basierend auf dem ausgewählten Angriff
+            // Schadensbestimmung auf dem ausgewählten Angriff
             val damage = when (selectedAttack) {
                 "Angriff 1" -> Random.nextInt(10, 20)
                 "Angriff 2" -> Random.nextInt(15, 25)
@@ -35,10 +34,10 @@ open class Held(val name: String) {
                 else -> Random.nextInt(5, 10)
             }
 
-            // Ausgabe einer Nachricht über den Angriff und den verursachten Schaden
+            // Gibt eine Nachricht über den Angriff und den verursachten Schaden
             println("$name greift ${enemy.name} mit $selectedAttack an und verursacht $damage Schaden.")
 
-            // Verursache den Schaden am Gegner
+            // Verursacht den Schaden am Gegner
             enemy.takeDamage(damage)
         } else {
             // Wenn die Eingabe ungültig ist, führe einen Standardangriff aus
@@ -47,7 +46,7 @@ open class Held(val name: String) {
             // Bestimmung des Schadens für den Standardangriff
             val damage = Random.nextInt(5, 10)
 
-            // Ausgabe einer Nachricht über den Standardangriff und den verursachten Schaden
+
             println("$name führt einen Standardangriff aus und verursacht $damage Schaden.")
 
             // Verursache den Schaden am Gegner
@@ -57,20 +56,20 @@ open class Held(val name: String) {
 
     // Methode zum Verwenden eines Gegenstands aus dem Inventar
     fun useItem(item: String) {
-        // Überprüfung, ob der Gegenstand im Inventar vorhanden ist
+        //Gegenstand im Inventar vorhanden ist
         if (inventory.contains(item)) {
-            // Ausgabe einer Nachricht über die Verwendung des Gegenstands
+            // Nachricht über die Verwendung des Gegenstands
             println("$name verwendet $item.")
 
-            // Entfernen des verwendeten Gegenstands aus dem Inventar
+            // Entfert Gegenstand aus dem Inventar
             inventory.remove(item)
 
-            // Verhalten basierend auf dem verwendeten Gegenstand
+
             when (item) {
                 "Heilungstrank" -> {
                     // Heilungsmenge für den Heiltrank
                     val healingAmount = 20
-                    // Heile den Helden, begrenzt auf maximal 100 Lebenspunkte
+                    // Heilt den Helden, begrenzt auf maximal 100 Lebenspunkte
                     health = minOf(health + healingAmount, 100)
                     // Ausgabe einer Nachricht über die Heilung
                     println("$name wird um $healingAmount Lebenspunkte geheilt.")
@@ -79,7 +78,7 @@ open class Held(val name: String) {
                 "Schutzzauber" -> println("$name ist geschützt und nimmt weniger Schaden.")
             }
         } else {
-            // Wenn der Gegenstand nicht im Inventar ist, gib eine entsprechende Nachricht aus
+            // Wenn der Gegenstand nicht im Inventar ist
             println("$name hat dieses Item nicht im Inventar.")
         }
     }
